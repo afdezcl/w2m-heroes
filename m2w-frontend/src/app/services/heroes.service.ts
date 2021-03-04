793a738aae45e95f280c2efd9130709c00c6ffe0
afdezcl <adrifdez_@uma.es>
2021-03-04 23:57:24 +0100

    W2M: Add crud methods on heroes service

--- m2w-frontend/src/app/services/heroes.service.ts
+++ m2w-frontend/src/app/services/heroes.service.ts
@@ -13,4 +13,16 @@ export class HeroesService {
     getHeroes() {
         return this.http.get(`${environment.apiURL}heroes`);
     }
+
+    getHeroById(id: number) {
+        return this.http.get(`${environment.apiURL}heroes/${id}`);
+    }
+
+    updateHero(idHero: number, heroToUpdate) {
+        return this.http.patch(`${environment.apiURL}heroes/${idHero}`, heroToUpdate);
+    }
+
+    deleteHero(idHero: number) {
+        return this.http.delete(`${environment.apiURL}heroes/${idHero}`);
+    }
 }
