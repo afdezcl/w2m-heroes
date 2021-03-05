94d1e98c4f135d3fd3a2306e9aaf867ece84b764
afdezcl <adrifdez_@uma.es>
2021-03-05 16:56:56 +0100

    W2M-2: Initial heroes view with flex layout responsive

--- m2w-frontend/src/app/components/heroes/heroes.module.ts
+++ m2w-frontend/src/app/components/heroes/heroes.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroesComponent } from './heroes.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
-
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+
+    FlexLayoutModule,
+
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   declarations: [HeroesComponent]
 })
