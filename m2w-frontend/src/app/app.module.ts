a357f1309d1e569921d4a2cb1cb10b40230f1743
afdezcl <adrifdez_@uma.es>
2021-03-05 15:37:54 +0100

    W2M-2: Add lazy loading module with a initial heroes component

--- m2w-frontend/src/app/app.module.ts
+++ m2w-frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,9 +13,7 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-
-
-    MatSliderModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ m2w-frontend/src/app/components/heroes/heroes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroesComponent } from './heroes.component';
+import { HeroesRoutingModule } from './heroes-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HeroesRoutingModule
+  ],
+  declarations: [HeroesComponent]
+})
+export class HeroesModule { }
